a91a3ba9653d4fda7b48239c5a065355863245e9
long <baolongp9@gmail.com>
2026-05-05 16:28:03 +0700

    set key for product

--- be-mobile/src/product/entities/product.entity.ts
+++ be-mobile/src/product/entities/product.entity.ts
@@ -1,63 +1,70 @@
+import { IsNotEmpty } from 'class-validator';
 import { TypeProduct } from 'src/common/enums/product.enum';
 import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  UpdateDateColumn,
-  PrimaryColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    PrimaryColumn,
 } from 'typeorm';
 @Entity()
-export class Product
-{
-    @PrimaryGeneratedColumn()
-    ProductId: string;
-
-    @Column({nullable: false})
-    ProductName: string;
-
-    @Column({nullable: false})
-    description: string;
-
-    @Column({nullable: false})
-    price: number;
-
-    @Column({nullable: false})
-    brand: string;
-
-    @Column({nullable: false})
-    quantity: number;
-
-    @Column({ enum:['New Product', 'Best Seller', 'Sale'], default: 'New Product'})
-    typeProduct: TypeProduct;
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    ProductId!: string;
 
     @Column()
-    imgUrl: string;
+    @IsNotEmpty()
+    ProductName!: string;
+
+    @Column()
+    @IsNotEmpty()
+    description!: string;
+
+    @Column()
+    @IsNotEmpty()
+    price!: number;
+
+    @Column()
+    @IsNotEmpty()
+    brand!: string;
+
+    @Column()
+    @IsNotEmpty()
+    quantity!: number;
+
+    @Column({ enum: ['New Product', 'Best Seller', 'Sale'], default: 'New Product' })
+    typeProduct!: TypeProduct;
+
+    @Column({ nullable: true })
+    imgUrl!: string;
 
     @Column({ type: 'json', nullable: true })
-    variants: ProductVariant[];
+    variants!: ProductVariant[];
 
     @CreateDateColumn()
-    createdAt: Date;
+    createdAt!: Date;
 
     @UpdateDateColumn()
     updatedAt?: Date;
 
 }
 
-export class ProductVariant
-{
+export class ProductVariant {
     @PrimaryColumn()
     id?: string;
 
     @Column()
-    label: string;
+    @IsNotEmpty()
+    label!: string;
 
     @Column()
-    price: number;
+    @IsNotEmpty()
+    price!: number;
 
     @Column()
-    quantity: number;
+    @IsNotEmpty()
+    quantity!: number;
 
     @Column()
     sku?: string;
